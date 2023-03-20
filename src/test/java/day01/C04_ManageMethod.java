bdeeeb9f5d2cab6e3ae9c87bb90b490ae5be6078
Aykut Gökce <aykutgokce2016@gmail.com>
2023-03-20 12:05:05 +0100

    day01DT

---
+++ src/test/java/day01/C04_ManageMethod.java
@@ -0,0 +1,43 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class C04_ManageMethod {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(ops);
+
+        //techproeducation sayfasina gidelim
+        driver.get("https://www.techproeducation.com/");
+
+        //sayfanin konumu ve boyutlarini yazdirin
+        System.out.println("Sayfanin Konumu: " + driver.manage().window().getPosition());
+        System.out.println("Sayfanin boyutlari: "+driver.manage().window().getSize());
+
+        //Sayfayi minimize (simge durunu) yapalim.
+        Thread.sleep(2000);
+        driver.manage().window().minimize();
+
+        //Sayfayi maximize(büyütelim) yapalim
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+
+        //sayfanin konumu ve boyutlarini yazdirin
+        System.out.println("Sayfanin Konumu: " + driver.manage().window().getPosition());
+        System.out.println("Sayfanin boyutlari: "+driver.manage().window().getSize());
+
+        //sayfayi full screen yapalim
+        Thread.sleep(2000);
+        driver.manage().window().fullscreen();
+
+        //sayfanin konumu ve boyutlarini yazdirin
+        System.out.println("Sayfanin Konumu: " + driver.manage().window().getPosition());
+        System.out.println("Sayfanin boyutlari: "+driver.manage().window().getSize());
+
+        driver.close();
+    }
+}
